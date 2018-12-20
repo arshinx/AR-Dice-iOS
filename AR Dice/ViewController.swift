67befe7a205c092c9c13912b6277239aba288e32
arshin <arshinx@users.noreply.github.com>
2018-12-19 18:20:37 -0800

    Create and style a cube

--- AR Dice/ViewController.swift
+++ AR Dice/ViewController.swift
@@ -20,6 +20,13 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         
+        // Cube
+        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.1)
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.red
+        
+        cube.materials = [material]
+        
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
@@ -47,29 +54,4 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
 
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
 }
